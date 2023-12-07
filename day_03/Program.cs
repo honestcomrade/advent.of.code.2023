870519d8c57d0d172340ec0abfc59f576e3ea999
Joe Saraceno <honestcomrade@gmail.com>
2023-12-06 20:54:32 -0700

    stub day 3

---
+++ day_03/Program.cs
@@ -0,0 +1,15 @@
+﻿using honestcomrade.advent.day03.services;
+
+namespace honestcomrade.advent.day03;
+public class Run
+{
+  public static void Main()
+  {
+    int total = 0;
+    var lines = new FileReader("./inputs/testinput.txt").ReadLines();
+    foreach (var line in lines)
+    {
+      Console.WriteLine(line);
+    }
+  }
+}
