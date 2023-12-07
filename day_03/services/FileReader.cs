5162f4bd327fb2c15a73b388e5261f94f8587fe9
Joe Saraceno <honestcomrade@gmail.com>
2023-12-06 20:56:34 -0700

    csharp 12 oooo

--- day_03/services/FileReader.cs
+++ day_03/services/FileReader.cs
@@ -1,13 +1,9 @@
 namespace honestcomrade.advent.day03.services;
-public class FileReader
+public class FileReader(string filePath)
 {
-  private readonly List<string> _lines = new();
-  private readonly string _filePath;
+  private readonly List<string> _lines = [];
+  private readonly string _filePath = filePath;
 
-  public FileReader(string filePath)
-  {
-    _filePath = filePath;
-  }
   public IEnumerable<string> ReadLines()
   {
     try
---
+++ day_03/services/LineReader.cs
@@ -0,0 +1,5 @@
+namespace honestcomrade.advent.day03.services;
+public class LineReader(string line)
+{
+  private readonly string _line = line;
+}
